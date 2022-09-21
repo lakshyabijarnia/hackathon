1e4893dffa3f0638b1584495cbf57811c22d5818
lakshyabijarnia <lakshyabijarnia@gmail.com>
2022-09-21 20:21:22 +0530

    done

---
+++ Python/Reading_and_Writing_files/readingfiles.py
@@ -0,0 +1,3 @@
+file=open("spider.txt","w")
+file.writelines("and washed the spider out .\nOut came the sun \nand dried up all the rain\nand the itsy bitsy spider climbed up the spout again .")
+file.close()
